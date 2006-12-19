a2ab8c3dc2a95948a217871c77ef89f145f14b45
frederik.deweerdt <frederik.deweerdt@47d29420-ef23-0410-9a09-917dd6f117e6>
2006-12-19 12:47:04 +0000

    Add libpthread calls hijacking, thanks to the phrack folks (issue #58) for the idea

--- dlock.c
+++ dlock.c
@@ -7,11 +7,30 @@
 #include <assert.h>
 #include <asm/atomic.h>
 #include <signal.h>
+#include <dlfcn.h>
+
 typedef void (*sighandler_t)(int);
 
 #include "dlock.h"
 
+/* Internal dump funcions for dlock hacking only */
+#undef LOCK_DEBUG
+#ifdef LOCK_DEBUG
+#define DLOCKprintf(fmt, args...) printf(fmt, args)
+#else
+#define DLOCKprintf(fmt, args...)
+#endif
+
+
 #ifdef DLOCK_ORDERING
+
+static void *libpthread_handle = NULL;
+
+static int (*o_pthread_mutex_init)(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
+static int (*o_pthread_mutex_lock)(pthread_mutex_t *);
+static int (*o_pthread_mutex_trylock)(pthread_mutex_t *);
+static int (*o_pthread_mutex_unlock)(pthread_mutex_t *);
+
 
 #if defined(__i386__) || defined(__x86_64__)
 static __inline__ unsigned long long int arch_get_ticks(void)
@@ -57,27 +76,120 @@ static unsigned long calibrating_loop()
 	return mhz;
 }
 
-void dlock_init(int flags)
+inline int ___pthread_mutex_destroy(pthread_mutex_t *mutex)
 {
-	if (DLOCK_INIT_CALIBRATE)
+	DLOCKprintf("destroying %d %p\n", (int)pthread_self(), mutex);
+	return pthread_mutex_destroy(mutex);
+}
+
+inline int ___pthread_mutex_init(pthread_mutex_t *mutex, void *v, char *mutex_name, char *fn, int ln)
+{
+	int ret;
+	DLOCKprintf("initing %d %p %s\n", (int)pthread_self(), mutex, mutex_name);
+	ret = o_pthread_mutex_init(mutex, v);	
+	ordering_init(mutex, v, mutex_name, fn, ln);
+	return ret;
+}
+
+inline int ___pthread_mutex_lock(pthread_mutex_t *mutex, char *lname, char *fn, int ln)
+{
+	int ret;
+	DLOCKprintf("locking %d %p %s_%s_%d\n", (int)pthread_self(), mutex, lname, fn, ln);
+	ret = o_pthread_mutex_lock(mutex);
+	ordering_lock(mutex, (int)pthread_self());
+	return ret;
+}
+
+inline int ___pthread_mutex_try_lock(pthread_mutex_t *mutex)
+{
+	DLOCKprintf("try locking %d %p\n", (int)pthread_self(), mutex);
+	return o_pthread_mutex_trylock(mutex);
+}
+
+inline int ___pthread_mutex_unlock(pthread_mutex_t *mutex, char *lname, char *fn, int ln)
+{
+	DLOCKprintf("unlocking %d %p %s_%s_%d\n", (int)pthread_self(), mutex, lname, fn, ln);
+	ordering_unlock(mutex, (int)pthread_self());
+	return o_pthread_mutex_unlock(mutex);
+}
+
+#define HIJACK(a, x, y) if (!(o_##x = dlsym(a , y))) {\
+			   fprintf(stderr, "symbol %s() not found, exiting\n", #y);\
+                	   exit(-1);\
+                        }
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr)
+{
+	char buf[16];
+	sprintf(buf, "%p", mutex);
+	ordering_init(mutex, (void *)attr, buf, "?", 0);
+	return o_pthread_mutex_init(mutex, attr);
+}
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+	ordering_lock(mutex, pthread_self());
+	return o_pthread_mutex_lock(mutex);
+}
+int pthread_mutex_trylock(pthread_mutex_t *mutex)
+{
+	return o_pthread_mutex_trylock(mutex);
+}
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+	ordering_unlock(mutex, pthread_self());
+	return o_pthread_mutex_unlock(mutex);
+}
+
+void dlock_hijack_libpthread()
+{
+	if ( (libpthread_handle = dlopen("libpthread.so", RTLD_NOW)) == NULL)
+		if ( (libpthread_handle = dlopen("libpthread.so.0", RTLD_NOW)) == NULL) {
+			fprintf(stderr, "error loading libpthread!\n");
+			exit(-1);
+		}
+	HIJACK(libpthread_handle, pthread_mutex_init, "pthread_mutex_init");
+	HIJACK(libpthread_handle, pthread_mutex_lock, "pthread_mutex_lock");
+	HIJACK(libpthread_handle, pthread_mutex_trylock, "pthread_mutex_trylock");
+	HIJACK(libpthread_handle, pthread_mutex_unlock, "pthread_mutex_unlock");
+}
+
+/* Called by ld */
+void _init()
+{
+	int flags;
+	const char *str_flags = getenv("DLOCK_FLAGS");
+	const char *preload = getenv("LD_PRELOAD");
+
+	if (!str_flags)
+		flags = 0;
+	else
+		flags = strtol(str_flags, NULL, 10);
+
+	if (preload && !str_flags) {
+		flags = DLOCK_INIT_HANDLE_USR1;
+	}
+
+	if (flags & DLOCK_INIT_CALIBRATE)
 		machine_mhz = calibrating_loop();
-	if (DLOCK_INIT_HANDLE_USR1)
+	if (flags & DLOCK_INIT_HANDLE_USR1)
 		signal(SIGUSR1, (sighandler_t)ordering_dump);
-	if (DLOCK_INIT_HANDLE_USR2)
+	if (flags & DLOCK_INIT_HANDLE_USR2)
 		signal(SIGUSR2, (sighandler_t)ordering_dump);
 				
+	dlock_hijack_libpthread();
+	fprintf(stderr, "libdlock loaded\n");
 }
 
 #undef ASSERT
-#define ASSERT(s, e, d) {					\
-	if (!(e)) {						\
-		printf("DLOCK ASSERT: %s %s %s:%d\n%s\n", 	\
-			#e, s, __FILE__, __LINE__,		\
-			(d)->last_used_bt);			\
-		bt();						\
-		fflush(stdout);					\
-		abort();					\
-	}							\
+#define ASSERT(s, e, d) {						\
+	if (!(e)) {							\
+		fprintf(stderr, "DLOCK ASSERT: %s %s %s:%d\n%s\n",	\
+			#e, s, __FILE__, __LINE__,			\
+			(d)->last_used_bt);				\
+		bt();							\
+		fflush(stdout);						\
+		abort();						\
+	}								\
 }
 
 typedef enum {
@@ -145,11 +257,11 @@ static void create_queue(unsigned int tid)
 		t = malloc(sizeof(struct thread_list));
 		LIST_INSERT_HEAD(&lhead, t, entries);
 	}
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	t->index = 0;
 	memset(t->seq, 0, sizeof(t->seq));
 	t->tid = tid;
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 }
 
 static void append_lock(unsigned int tid, pthread_mutex_t *mutex) 
@@ -157,12 +269,12 @@ static void append_lock(unsigned int tid, pthread_mutex_t *mutex)
 	struct thread_list *t;
 
 	t = find_list(tid);
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	t->seq[t->index].mutex = mutex;
 	t->seq[t->index].mtime = arch_get_ticks();
 	t->seq[t->index].action = LOCKED;
 	t->index++;
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 }
 
 static int append_unlock(unsigned int tid, pthread_mutex_t *mutex) 
@@ -171,14 +283,14 @@ static int append_unlock(unsigned int tid, pthread_mutex_t *mutex)
 	int ret;
 
 	t = find_list(tid);
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	t->seq[t->index].mutex = mutex;
 	t->seq[t->index].mtime = arch_get_ticks();
 	t->seq[t->index].action = UNLOCKED;
 	t->index++;
 	/* loop is closed? */
 	ret = t->seq[0].mutex == mutex;
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 	return ret;
 }
 
@@ -191,14 +303,14 @@ void ordering_init(pthread_mutex_t * mutex, void *v,
 	if (mutex_name[0] == '&') {
 		skip = 1;
 	}
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	mutex_descs[md_index].mutex = mutex;
 	strncpy(mutex_descs[md_index].mutex_name, mutex_name + skip,
 		MAX_MUTEX_NAME);
 	strncpy(mutex_descs[md_index].fn_name, fn, MAX_FN_NAME);
 	mutex_descs[md_index].ln = ln;
 	md_index++;
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 }
 void ordering_lock(pthread_mutex_t *mutex, unsigned int tid)
 {
@@ -232,7 +344,7 @@ static void register_sequence(unsigned int tid)
 	unsigned long long mtime;
 
 	t = find_list(tid);
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	memcpy(sequences[atomic_read(&index_sequences)].seq, t->seq, 
 		sizeof(t->seq));
 	sequences[atomic_read(&index_sequences)].cksum = cksum(tid);
@@ -241,7 +353,7 @@ static void register_sequence(unsigned int tid)
 	sequences[atomic_read(&index_sequences)].times = 1;
 	sequences[atomic_read(&index_sequences)].length = t->index;
 	atomic_inc(&index_sequences);
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 	return;
 }
 
@@ -309,7 +421,7 @@ void ordering_dump()
 {
 	int i, j;
 
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	printf
 	    ("********************************************************************************\n");
 	printf("Registered lock sequences:\n");
@@ -354,7 +466,7 @@ void ordering_dump()
 		       mutex_descs[i].fn_name, mutex_descs[i].ln);
 	}
 
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 }
 
 //TODO: does not work properly
@@ -362,7 +474,7 @@ void ordering_gen_dot()
 {
 	int i, j;
 	printf("digraph g {\n");
-	pthread_mutex_lock(&ordering_mutex);
+	o_pthread_mutex_lock(&ordering_mutex);
 	for (i = 0; i < atomic_read(&index_sequences); i++) {
 		int first = 1;
 		for (j = 0; j < sequences[i].length; j++) {
@@ -378,7 +490,7 @@ void ordering_gen_dot()
 		}
 		printf(";\n");
 	}
-	pthread_mutex_unlock(&ordering_mutex);
+	o_pthread_mutex_unlock(&ordering_mutex);
 	printf("}\n");
 }
 #endif /* DLOCK_ORDERING */
