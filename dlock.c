8947d62382f7cb08b4fe63b703ea5e18fbd450b3
frederik.deweerdt <frederik.deweerdt@47d29420-ef23-0410-9a09-917dd6f117e6>
2007-01-02 21:23:39 +0000

    Rewrite to support the generic lock_t type Move defines and enums to dlock_core.h

--- dlock.c
+++ dlock.c
@@ -32,44 +32,15 @@
 typedef void (*sighandler_t)(int);
 
 #include "dlock.h"
+#include "dlock_core.h"
+#include "pthread_spinlocks.h"
+#include "pthread_mutexes.h"
 #include "tree.h"
 
-/* Internal dump funcions for dlock hacking only */
-#undef LOCK_DEBUG
-#ifdef LOCK_DEBUG
-#define DLOCKprintf(fmt, args...) printf(fmt, args)
-#else
-#define DLOCKprintf(fmt, args...)
-#endif
-
-#undef ASSERT
-#define ASSERT(s, e, d) {						\
-	if (!(e)) {							\
-		fprintf(stderr, "DLOCK ASSERT: %s %s %s:%d\n%s\n",	\
-			#e, s, __FILE__, __LINE__,			\
-			(d)->last_used_bt);				\
-		bt();							\
-		fflush(stderr);						\
-		abort();						\
-	}								\
-}
-
-void dlock_mutex_init(pthread_mutex_t *mutex, void *v, const char *mutex_name, char *fn, int ln);
-void dlock_mutex_lock(pthread_mutex_t *mutex, unsigned int tid);
-void dlock_mutex_unlock(pthread_mutex_t *mutex, unsigned int tid);
-void dlock_dump();
-void dlock_gen_dot();
-
-
-static void *libpthread_handle = NULL;
+/** Log file, stderr by default */
 static FILE *dlock_log_file;
 
-static int (*o_pthread_mutex_init)(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
-static int (*o_pthread_mutex_lock)(pthread_mutex_t *);
-static int (*o_pthread_mutex_trylock)(pthread_mutex_t *);
-static int (*o_pthread_mutex_unlock)(pthread_mutex_t *);
-
-static struct mutex_desc *get_mutex_desc(pthread_mutex_t * m);
+static struct lock_desc *get_lock_desc(lock_t *lock);
 
 #if defined(__i386__) || defined(__x86_64__)
 static __inline__ unsigned long long int arch_get_ticks(void)
@@ -82,14 +53,14 @@ static __inline__ unsigned long long int arch_get_ticks(void)
 #error "Unsupported arch, please report to frederik.deweerdt@gmail.com"
 #endif /* arch_get_ticks */
 
-/* Maximum number of mutex/action pairs recorded in a lock chain */
+/* Maximum number of lock/action pairs recorded in a lock chain */
 #define MAX_LOCK_DEPTH 20
 /* Number of different lock sequences recorded */
 #define MAX_LOCK_SEQ 1024
-/* Number of different mutexes known to the system */
-#define MAX_KNOWN_MUTEXES 1024
-/* Length of the recorded name of a mutex */
-#define MAX_MUTEX_NAME 255
+/* Number of different locks known to the system */
+#define MAX_KNOWN_LOCKS 1024
+/* Length of the recorded name of a lock */
+#define MAX_LOCK_NAME 255
 #define MAX_FN_NAME 255
 
 static unsigned long machine_mhz = ~0UL;
@@ -115,72 +86,9 @@ static unsigned long calibrating_loop()
 	return mhz;
 }
 
-int ___pthread_mutex_destroy(pthread_mutex_t *mutex)
-{
-	DLOCKprintf("destroying %d %p\n", (int)pthread_self(), mutex);
-	return pthread_mutex_destroy(mutex);
-}
-
-int ___pthread_mutex_init(pthread_mutex_t *mutex, void *v, char *mutex_name, char *fn, int ln)
-{
-	int ret;
-	DLOCKprintf("initing %d %p %s\n", (int)pthread_self(), mutex, mutex_name);
-	ret = o_pthread_mutex_init(mutex, v);	
-	dlock_mutex_init(mutex, v, mutex_name, fn, ln);
-	return ret;
-}
-
-int ___pthread_mutex_lock(pthread_mutex_t *mutex, char *lname, char *fn, int ln)
-{
-	int ret;
-	DLOCKprintf("locking %d %p %s_%s_%d\n", (int)pthread_self(), mutex, lname, fn, ln);
-	ret = o_pthread_mutex_lock(mutex);
-	dlock_mutex_lock(mutex, (int)pthread_self());
-	return ret;
-}
-
-int ___pthread_mutex_try_lock(pthread_mutex_t *mutex)
-{
-	DLOCKprintf("try locking %d %p\n", (int)pthread_self(), mutex);
-	return o_pthread_mutex_trylock(mutex);
-}
-
-int ___pthread_mutex_unlock(pthread_mutex_t *mutex, char *lname, char *fn, int ln)
-{
-	DLOCKprintf("unlocking %d %p %s_%s_%d\n", (int)pthread_self(), mutex, lname, fn, ln);
-	dlock_mutex_unlock(mutex, (int)pthread_self());
-	return o_pthread_mutex_unlock(mutex);
-}
-
-#define HIJACK(a, x, y) if (!(o_##x = dlsym(a , y))) {\
-			   fprintf(dlock_log_file, "symbol %s() not found, exiting\n", #y);\
-                	   exit(-1);\
-                        }
-
-int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr)
-{
-	char buf[16];
-	sprintf(buf, "%p", mutex);
-	dlock_mutex_init(mutex, (void *)attr, buf, "?", 0);
-	return o_pthread_mutex_init(mutex, attr);
-}
-int pthread_mutex_lock(pthread_mutex_t *mutex)
-{
-	dlock_mutex_lock(mutex, pthread_self());
-	return o_pthread_mutex_lock(mutex);
-}
-int pthread_mutex_trylock(pthread_mutex_t *mutex)
-{
-	return o_pthread_mutex_trylock(mutex);
-}
-int pthread_mutex_unlock(pthread_mutex_t *mutex)
-{
-	dlock_mutex_unlock(mutex, pthread_self());
-	return o_pthread_mutex_unlock(mutex);
-}
-
 void dlock_hijack_libpthread()
 {
+	void *libpthread_handle = NULL;
 	if ( (libpthread_handle = dlopen("libpthread.so", RTLD_NOW)) == NULL)
 		if ( (libpthread_handle = dlopen("libpthread.so.0", RTLD_NOW)) == NULL) {
 			fprintf(dlock_log_file, "error loading libpthread!\n");
@@ -190,18 +98,12 @@ void dlock_hijack_libpthread()
 	HIJACK(libpthread_handle, pthread_mutex_lock, "pthread_mutex_lock");
 	HIJACK(libpthread_handle, pthread_mutex_trylock, "pthread_mutex_trylock");
 	HIJACK(libpthread_handle, pthread_mutex_unlock, "pthread_mutex_unlock");
+
+	HIJACK(libpthread_handle, pthread_spin_init, "pthread_spin_init");
+	HIJACK(libpthread_handle, pthread_spin_lock, "pthread_spin_lock");
+	HIJACK(libpthread_handle, pthread_spin_trylock, "pthread_spin_trylock");
+	HIJACK(libpthread_handle, pthread_spin_unlock, "pthread_spin_unlock");
 }
-
-enum pthread_state {
-	LOCKED,
-	UNLOCKED,
-};
-
-struct lock_action {
-	pthread_mutex_t *mutex;
-	enum pthread_state action;
-	unsigned long long mtime;
-};
 
 struct thread_list {
 	unsigned int tid;
@@ -218,18 +120,19 @@ struct sequence {
 	struct dlock_node *root;
 };
 
-struct mutex_desc {
-	pthread_mutex_t *mutex;
-	char name[MAX_MUTEX_NAME];
+struct lock_desc {
+	lock_t *lock;
+	char name[MAX_LOCK_NAME];
 	char fn_name[MAX_FN_NAME];
 	int ln;
-	pthread_mutex_t *depends_on[MAX_KNOWN_MUTEXES];
+	lock_t *depends_on[MAX_KNOWN_LOCKS];
+	enum lock_type type;
 };
 
-/* Holds the known mutex_desc entries */
-static struct mutex_desc mutex_descs[MAX_KNOWN_MUTEXES];
-/* current index in the mutex_descs array */
-static unsigned int md_index = 0;
+/* Holds the known lock_desc entries */
+static struct lock_desc lock_descs[MAX_KNOWN_LOCKS];
+/* current index in the lock_descs array */
+static unsigned int ld_index = 0;
 /* Big DLOCK lock. todo: find a more fine grained locking scheme*/
 static pthread_mutex_t descs_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t thread_list_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -322,9 +225,9 @@ static struct thread_list *create_queue(unsigned int tid)
  *
  * @return the parent mutex or NULL if no parent mutex was found
  **/
-static pthread_mutex_t *get_previous_lock(struct thread_list *t)
+static lock_t *get_previous_lock(struct thread_list *t)
 {
-	return t->current_node->parent->mutex;
+	return t->current_node->parent->lock;
 }
 
 /**
@@ -336,17 +239,17 @@ static pthread_mutex_t *get_previous_lock(struct thread_list *t)
  *
  * @return 
  **/
-static void new_dependency(struct mutex_desc *desc, pthread_mutex_t *master_mutex)
+static void new_dependency(struct lock_desc *desc, lock_t *master_mutex)
 {
 	int i;
 	
 	/* skip to an empty stop in depends_on[] */
 	for (i=0; desc->depends_on[i] != master_mutex 
 		  && desc->depends_on[i] != NULL 
-		  && i < MAX_KNOWN_MUTEXES; i++);
+		  && i < MAX_KNOWN_LOCKS; i++);
 
 	/* sanity check */
-	assert(i != MAX_KNOWN_MUTEXES && "Too much mutexes");
+	assert(i != MAX_KNOWN_LOCKS && "Too much mutexes");
 
 	/* known dependency, don't add */
 	if (desc->depends_on[i] == master_mutex)
@@ -357,29 +260,29 @@ static void new_dependency(struct mutex_desc *desc, pthread_mutex_t *master_mute
 }
 
 /**
- * @brief Tests whenever @mutex depends on @probed
+ * @brief Tests whenever @mutex depends on @probed_lock
  *
  * @param mutex  
- * @param probed  
+ * @param probed_lock
  *
- * @return 1 if @mutex depends on probed, 0 otherwise
+ * @return 1 if @mutex depends on probed_lock, 0 otherwise
  **/
-static int is_depending_on(pthread_mutex_t *mutex, pthread_mutex_t *probed)
+static int is_depending_on(lock_t *lock, lock_t *probed_lock)
 {
 	int i;
-	struct mutex_desc *desc;
+	struct lock_desc *desc;
 
-	if (mutex == probed) {
+	if (lock == probed_lock) {
 		return 1;
 	}
 
-	desc = get_mutex_desc(mutex);
+	desc = get_lock_desc(lock);
 
-	for (i=0; desc->depends_on[i] != NULL && i < MAX_KNOWN_MUTEXES; i++) {
-		if (desc->depends_on[i] == probed) {
+	for (i=0; desc->depends_on[i] != NULL && i < MAX_KNOWN_LOCKS; i++) {
+		if (desc->depends_on[i] == probed_lock) {
 			return 1;
 		}
-		if (is_depending_on(desc->depends_on[i], probed)) {
+		if (is_depending_on(desc->depends_on[i], probed_lock)) {
 			return 1;
 		}
 	}
@@ -387,9 +290,9 @@ static int is_depending_on(pthread_mutex_t *mutex, pthread_mutex_t *probed)
 }
 
 #define ALLOC_STEP 4
-static void append_lock(struct thread_list *t, pthread_mutex_t *mutex) 
+static void append_lock(struct thread_list *t, lock_t *lock) 
 {
-	pthread_mutex_t *previous;
+	lock_t *previous;
 
 	t->current_node->nb_children++;
 	#if 1
@@ -404,7 +307,7 @@ static void append_lock(struct thread_list *t, pthread_mutex_t *mutex)
 	#endif
 	CUR_CHILD(t) = calloc(1, sizeof(struct dlock_node));
 	CUR_CHILD(t)->parent =  t->current_node;
-	CUR_CHILD(t)->mutex =  mutex;
+	CUR_CHILD(t)->lock =  lock;
 	CUR_CHILD(t)->lock_time = arch_get_ticks();
 
 	t->current_node = CUR_CHILD(t);
@@ -414,46 +317,54 @@ static void append_lock(struct thread_list *t, pthread_mutex_t *mutex)
 	if (previous) {
 		/* let us know if the previous lock was already
 		 * depending on mutex (deadlock?)*/
-		if (is_depending_on(previous, mutex)) {
+		if (is_depending_on(previous, lock)) {
 			dlock_dump();
-			fprintf(dlock_log_file, "New dependency %p -> %p leads to deadlock\n", previous, mutex);
+			fprintf(dlock_log_file, "New dependency %p -> %p leads to deadlock\n", previous, lock);
 			exit(0);
 		}
-		new_dependency(get_mutex_desc(mutex), previous);
+		new_dependency(get_lock_desc(lock), previous);
 	}
 }
 
-static int append_unlock(struct thread_list *t, pthread_mutex_t *mutex) 
+static int append_unlock(struct thread_list *t, lock_t *lock) 
 {
 	t->current_node->unlock_time = arch_get_ticks();
 	t->current_node = t->current_node->parent;	
 
 	/* is loop closed? */
 	return (t->root->nb_children 
-		&& FIRST_NODE(t)->mutex == mutex);
+		&& FIRST_NODE(t)->lock == lock);
 }
 
-void dlock_mutex_init(pthread_mutex_t * mutex, void *v,
-		   const char *mutex_name, char *fn, int ln)
+void __dlock_lock_init(lock_t *lock, void *v,
+		       const char *lock_name, char *fn, int ln,
+		       enum lock_type type)
 {
 	int skip = 0;
 
 	/* Skip the leading ampersand for aesthetic purposes */
-	if (mutex_name[0] == '&') {
+	if (lock_name[0] == '&') {
 		skip = 1;
 	}
-	o_pthread_mutex_lock(&descs_mutex);
-	mutex_descs[md_index].mutex = mutex;
-	strncpy(mutex_descs[md_index].name, mutex_name + skip,
-		MAX_MUTEX_NAME);
-	strncpy(mutex_descs[md_index].fn_name, fn, MAX_FN_NAME);
-	mutex_descs[md_index].ln = ln;
+	lock_descs[ld_index].lock = lock;
+	strncpy(lock_descs[ld_index].name, lock_name + skip,
+		MAX_LOCK_NAME);
+	strncpy(lock_descs[ld_index].fn_name, fn, MAX_FN_NAME);
+	lock_descs[ld_index].ln = ln;
 
-	md_index++;
+	ld_index++;
+}
+
+void dlock_lock_init(lock_t *lock, void *v,
+		     const char *lock_name, char *fn, int ln,
+		     enum lock_type type)
+{
+	o_pthread_mutex_lock(&descs_mutex);
+	__dlock_lock_init(lock, v, lock_name, fn, ln, type);
 	o_pthread_mutex_unlock(&descs_mutex);
 }
 
-void dlock_mutex_lock(pthread_mutex_t *mutex, unsigned int tid)
+void dlock_lock(lock_t *lock, unsigned int tid)
 {
 	struct thread_list *t;
 
@@ -461,7 +372,7 @@ void dlock_mutex_lock(pthread_mutex_t *mutex, unsigned int tid)
 	if (!t) {
 		t = create_queue(tid);
 	}
-	append_lock(t, mutex);
+	append_lock(t, lock);
 }
 
 static void register_sequence(struct thread_list *t)
@@ -511,7 +422,7 @@ out:
 	return ret;
 }
 
-void dlock_mutex_unlock(pthread_mutex_t *mutex, unsigned int tid)
+void dlock_unlock(lock_t *lock, unsigned int tid)
 {
 	struct thread_list *t;
 
@@ -521,7 +432,7 @@ void dlock_mutex_unlock(pthread_mutex_t *mutex, unsigned int tid)
 		assert(0);
 	}
 	/* if loop closed */
-	if (append_unlock(t, mutex)) {
+	if (append_unlock(t, lock)) {
 		/* do we already have such a sequence ? */
 		if(!is_registered_sequence(t)) {
 			register_sequence(t);
@@ -536,20 +447,25 @@ void dlock_mutex_unlock(pthread_mutex_t *mutex, unsigned int tid)
 	}
 }
 
-static struct mutex_desc *get_mutex_desc(pthread_mutex_t * m)
+static struct lock_desc *get_lock_desc(lock_t *lock)
 {
 	int i;
+	char buf[16];
+	struct lock_desc *ret;
 
 	o_pthread_mutex_lock(&descs_mutex);
-	for (i = 0; mutex_descs[i].mutex != 0 && i < MAX_KNOWN_MUTEXES; i++) {
-		if (m == mutex_descs[i].mutex) {
+	for (i = 0; lock_descs[i].lock != 0 && i < MAX_KNOWN_LOCKS; i++) {
+		if (lock == lock_descs[i].lock) {
 			o_pthread_mutex_unlock(&descs_mutex);
-			return &mutex_descs[i];
+			return &lock_descs[i];
 		}
 	}
+	sprintf(buf, "%p", lock);
+	__dlock_lock_init(lock, NULL, buf, "?", 0, UNKNOWN);
+	ret = &lock_descs[ld_index];
 	o_pthread_mutex_unlock(&descs_mutex);
-	assert(0);
-	return NULL;
+
+	return ret;
 }
 
 void print_lock_tree(struct dlock_node *n, int depth)
@@ -566,7 +482,7 @@ void print_lock_tree(struct dlock_node *n, int depth)
 	}
 	/* special case, skip root */
 	if (depth) {
-		char *name = get_mutex_desc(n->mutex)->name;
+		char *name = get_lock_desc(n->lock)->name;
 		char *locked = "";
 
 		/* display that the lock is held */
@@ -611,20 +527,20 @@ void dlock_dump()
 	}
 
 	/* The mutexes need to be inited with pthread_mutex_init to be printed here */
-	if (md_index > 0) {
+	if (ld_index > 0) {
 
 		fprintf (dlock_log_file,
 		     "********************************************************************************\n");
 
 		fprintf(dlock_log_file, "Mutexes known to dlock:\n");
-		for (i = 0; i < md_index; i++) {
+		for (i = 0; i < ld_index; i++) {
 			fprintf(dlock_log_file, "\t%p %s=%s:%d\n",
-				mutex_descs[i].mutex,
-				get_mutex_desc(mutex_descs[i].mutex)->name,
-				mutex_descs[i].fn_name, mutex_descs[i].ln);
+				lock_descs[i].lock,
+				get_lock_desc(lock_descs[i].lock)->name,
+				lock_descs[i].fn_name, lock_descs[i].ln);
 			fprintf(dlock_log_file, "\t\tdepends on:\n");
-			for (j=0; mutex_descs[i].depends_on[j] != NULL; j++) {
-				fprintf(dlock_log_file, "\t\t%p\n", mutex_descs[i].depends_on[j]);
+			for (j=0; lock_descs[i].depends_on[j] != NULL; j++) {
+				fprintf(dlock_log_file, "\t\t%p\n", lock_descs[i].depends_on[j]);
 			}
 		}
 	}
@@ -642,7 +558,7 @@ void dlock_gen_dot()
 		for (j = 0; j < tsequences[i].length; j++) {
 			if (tsequences[i].seq[j].action == LOCKED) {
 				char *name =
-					get_mutex_desc(sequences[i].seq[j].mutex)->name;
+					get_lock_desc(sequences[i].seq[j].mutex)->name;
 				if (first)
 					first = 0;
 				else
